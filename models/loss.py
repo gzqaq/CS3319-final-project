ca052b53e2164ff5adc982c28a21c7e946db58c5
Ziqin Gong <gongzq0301@sjtu.edu.cn>
2023-06-11 19:33:28 +0800

    Add code for experiments

--- models/loss.py
+++ models/loss.py
@@ -1,3 +1,4 @@
+import torch
 import torch.nn.functional as F
 
 
@@ -12,3 +13,10 @@ def max_margin_loss(pos_score,
   scores = neg_score_tensor + delta - pos_score_tensor
 
   return F.relu(scores).mean()
+
+
+def binary_cross_entropy_loss(pos_score, neg_score, etype):
+  score = torch.cat([pos_score[etype], neg_score[etype]])
+  label = torch.cat([torch.ones_like(pos_score[etype]), torch.zeros_like(neg_score[etype])])
+
+  return F.binary_cross_entropy_with_logits(score, label)
